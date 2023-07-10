b7016a5cdfbdf92d94539a3427684c106e4f5336
ihsan YILMAZ <arkihekQA@gmail.com>
2023-07-10 12:15:04 +0300

    Day 13

---
+++ src/test/java/pages/FacebookPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class FacebookPage {
+
+    public FacebookPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(id="email")
+    public WebElement postaKutusu;
+
+    @FindBy(id="pass")
+    public WebElement sifreKutusu;
+
+    @FindBy(name = "login")
+    public WebElement loginKutusu;
+
+    @FindBy(xpath = "//*[@class='_9ay7']")
+    public WebElement hataMesaji;
+}
